9d5daa2798355139f750cfe447474b32b5e05c6a
Andrei Copaceanu <andrei.copaceanu@yahoo.com>
2022-03-03 12:22:40 +0000

    Bullet
    The bullet was added. The bullet and the case are physics based and are instantiated when the player is shooting.

--- Assets/Scripts/GunManager.cs
+++ Assets/Scripts/GunManager.cs
@@ -6,7 +6,18 @@ public class GunManager : MonoBehaviour
 {
     public bool shoot;
 
+    [SerializeField] Transform bulletSpawn;
+    [SerializeField] Transform caseSpawn;
+
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] GameObject casePrefab;
+
     [SerializeField] GameObject muzzleFlash;
+
+    [SerializeField] Vector3 bulletForce;
+    [SerializeField] Vector3 caseForce;
+
+    bool canShoot;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +27,45 @@ public class GunManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(shoot)
+        MuzzleFlash();
+    }
+
+    private void FixedUpdate()
+    {
+        Shoot();
+    }
+
+    void MuzzleFlash()
+    {
+        if (shoot)
         {
             muzzleFlash.SetActive(true);
         }
+
         else
         {
             muzzleFlash.SetActive(false);
+        }
+    }
+
+    void Shoot()
+    {
+        if (shoot)
+        {
+            if (canShoot)
+            {
+                Rigidbody bulletRB = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity).GetComponent<Rigidbody>();
+                Rigidbody caseRB = Instantiate(casePrefab, caseSpawn.position, Quaternion.identity).GetComponent<Rigidbody>();
+
+                bulletRB.AddRelativeForce(transform.forward * bulletForce.z);
+                caseRB.AddRelativeForce(transform.right * caseForce.x + transform.up * caseForce.y);
+
+                canShoot = false;
+            }
+        }
+        else
+        {
+            canShoot = true;
         }
     }
 }
